a5226c46fa241e2e9f9c479e8f330b5f8d90863e
Ferxcs <jferxcs@gmail.com>
2024-04-29 13:50:21 -0500

    iniciando el contro de versiones de mi primera app en Java

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args) {
+
+        System.out.println("Hola, Mundo!");
+        System.out.println("Pelicula: Matrix");
+     
+        //Comentario
+        //Tipos de variables
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (notaDeLaPelicula + 7.3 + 9.1) / 3;
+
+        String sinopsis = """
+                Matrix es una paradoja 
+                La mejor pelicula del fin del milenio
+                Fue lanzada en: 
+                """ + fechaDeLanzamiento;
+        //vamos a concatenar
+
+        System.out.println(sinopsis);
+    }
+}
